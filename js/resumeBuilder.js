7264e34fae2d2888fc44d9450a862d22d09dcf98
datasciencefan <datasciencefan@gmail.com>
2015-10-14 16:47:55 -0700

    updated bio, work, education variables

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,46 +6,102 @@ var skills = ["Data Analysis", "Machine Learning", "Visualization", "Web Scrapin
 var bio = {
 	"name" : "Thomas Wong",
  	"role" : "Data Analyst",
- 	"email" : "thomaswong@berkeley.edu",
- 	"bioPic" : "images/profilePic.jpg",
- 	"welcomeMessage" : "Nice to meet you!",
+ 	"contact" : {
+ 		"email" : "thomaswong@berkeley.edu",
+ 		"phone" : "510-499-2610",
+ 		"location" : "Berkeley, CA, US"
+ 	},
+ 	"pic" : "images/profilePic.jpg",
+ 	"message" : "Nice to meet you!",
  	"skills" : skills
  };
 
- //$("#main").append(bio.name + '&#xA;', bio.role + '&#xA;', bio.email + '&#xA;', bio.welcomeMessage + '&#xA;', bio.skills + '&#xA;');
- //$("#main").prepend(bio.bioPic + '&#xA;');
+ //$("#main").append(bio.name, bio.role, bio.email, bio.welcomeMessage, bio.skills);
+ //$("#main").prepend(bio.bioPic);
 
 var work = {
 	"jobs": [
 	{
-		"employer": "John Hopkins University",
-		"title": "Baltimore, MD, US",
-		"location": "Data Science Certification",
-		"dates": "Data Science",
-		"description": "Data Science"
+		"employer": "Leidos",
+		"title": "Data Analyst",
+		"location": "Oakland, CA, US",
+		"dates": "Jul 2014 - Present",
+		"description": "Data collecting, cleaning, processing, and analysis for over 30 transit agencies in the Bay Area"
 	},
 	{
-		"name": "UC Berkeley",
-		"city": "Berkeley, CA, US",
-		"degree": "MS",
-		"major": "Transportation Engineering"
+		"employer": "Seed",
+		"title": "Hardware and Software Engineer",
+		"location": "Hayward, CA, US",
+		"dates": "Jan 2015 - Jun 2015",
+		"description": "Hardware installation, software debugging, finances for startup"
 	}
 	]
-}
+};
 
 var education = {
 	"schools": [
 	{
 		"name": "John Hopkins University",
-		"city": "Baltimore, MD, US",
+		"location": "Baltimore, MD, US",
 		"degree": "Data Science Certification",
-		"major": "Data Science"
+		"major": "Data Science",
+		"dates": "May 2014 - May 2015",
+		"url" : "www.udacity.com"
 	},
 	{
 		"name": "UC Berkeley",
-		"city": "Berkeley, CA, US",
+		"location": "Berkeley, CA, US",
 		"degree": "MS",
-		"major": "Transportation Engineering"
+		"major": "Transportation Engineering",
+		"dates": "Aug 2013 - Dec 2014",
+		"url" : "www.udacity.com"
+	}
+	],
+	"online courses": [
+	{
+		"title": "MS",
+		"school": "Transportation Engineering",
+		"dates": "Aug 2013 - Dec 2014",
+		"url" : "www.udacity.com"
 	}
 	]
-}
+};
+
+var projects = {
+	"projects": [
+	{
+		"title": "Next Word Predictor",
+		"dates": "Dec 2014 - May 2015",
+		"description": "Predict next word (like SwiftKey). Capstone project for Data Science Certification.",
+		"images" : []
+	},
+	{
+		"title": "Next Word Predictor",
+		"dates": "Dec 2014 - May 2015",
+		"description": "Predict next word (like SwiftKey). Capstone project for Data Science Certification.",
+		"images" : []
+	}
+	]
+};
+
+var formattedName = HTMLheaderName.replace('%data%', bio.name);
+var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+var formattedPic = HTMLbioPic.replace('%data%', bio.pic);
+var formattedMessage = HTMLwelcomeMsg.replace('%data%', bio.message);
+
+var formattedEmail = HTMLemail.replace('%data%', bio.contact.email);
+var formattedMobile = HTMLmobile.replace('%data%', bio.contact.phone);
+var formattedLocation = HTMLlocation.replace('%data%', bio.contact.location);
+
+$('#header').append(formattedName, formattedRole, formattedPic, formattedMessage);
+$('#topContacts').append(formattedMobile, formattedEmail, formattedLocation);
+
+if(bio.skills.length > 0){
+$('#header').append(HTMLskillsStart);
+
+for(skill in bio.skills){
+	HTMLskills = HTMLskills.replace('%data%', bio.skills[skill]);
+	$('#skills').append(HTMLskills);
+	HTMLskills = HTMLskills.replace(bio.skills[skill], '%data%');
+};
+};
