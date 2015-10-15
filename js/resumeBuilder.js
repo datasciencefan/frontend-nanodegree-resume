310036e9f1262a872e7548c0cfd83ef88a873f6e
datasciencefan <datasciencefan@gmail.com>
2015-10-15 13:00:05 -0700

    add instructions for next steps

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -93,15 +93,72 @@ var formattedEmail = HTMLemail.replace('%data%', bio.contact.email);
 var formattedMobile = HTMLmobile.replace('%data%', bio.contact.phone);
 var formattedLocation = HTMLlocation.replace('%data%', bio.contact.location);
 
+var formattedEducation = HTMLeducation.replace('%data%', education);
+var formattedProjects = HTMLprojects.replace('%data%', projects);
+
+var HTMLheaderName = '<h1 id="name">%data%</h1>';
+var HTMLheaderRole = '<span>%data%</span><hr/>';
+
+/* use the below with the var lines above and the classes below for appending to add all info!
+
+var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+
+var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
+
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectImage = '<img src="%data%">';
+
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+
+var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div>';
+var HTMLonlineURL = '<br><a href="#">%data%</a>';
+
+var internationalizeButton = '<button>Internationalize</button>';
+var googleMap = '<div id="map"></div>';
+
+*/
+
 $('#header').append(formattedName, formattedRole, formattedPic, formattedMessage);
-$('#topContacts').append(formattedMobile, formattedEmail, formattedLocation);
+$('#lets-connect').append(formattedMobile, formattedEmail, formattedLocation);
 
 if(bio.skills.length > 0){
-$('#header').append(HTMLskillsStart);
+$('#skills-h3').append(HTMLskillsStart);
 
 for(skill in bio.skills){
 	HTMLskills = HTMLskills.replace('%data%', bio.skills[skill]);
-	$('#skills').append(HTMLskills);
+	$('#skills-h3').append(HTMLskills);
 	HTMLskills = HTMLskills.replace(bio.skills[skill], '%data%');
 };
-};
+
+$('#workExperience').append(formattedEducation);
+$('#projects').append(formattedProjects);
+
+}; // end of file
